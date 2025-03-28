293502bbadc88b95f335a749262cdc3b1576319a
dhanapal1410 <125759600+dhanapal1410@users.noreply.github.com>
2025-03-28 13:44:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "google_compute_network" "vpcnet" {
+  name = "terraform-network"
+}
+
+resource "google_compute_subnetwork" "vpcsubnet" {
+  name          = "terraform-subnetwork"
+  network       = google_compute_network.vpcnet.name
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+}
+
+resource "google_compute_firewall" "gfirewall" {
+  name    = "allow-http"
+  network = google_compute_network.vpcnet.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "ginstance" {
+  name         = "web-server"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpcnet.name
+    subnetwork = google_compute_subnetwork.vpcsubnet.name
+    access_config {}
+  }
+
+  metadata_startup_script = file("startup-script.sh")
+}
